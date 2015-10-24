36cd1719222c4d42f0103234edff1d014a8d3b4d
David Lima <dave@blogdodave.com.br>
2015-10-23 22:09:04 -0200

    Initial commit with some views Views available: index login register

---
+++ SplatGroups/README.md
@@ -0,0 +1,4 @@
+SplatGroups
+===========
+
+A Symfony project created on October 23, 2015, 8:03 pm.
---
+++ SplatGroups/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig', array(
+            
+        ));
+    }
+    
+    /**
+     * @Route("/login/", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        return $this->render('default/login.html.twig', array(
+        ));
+    }
+    
+    /**
+     * @Route("/register/", name="register") 
+     */
+    public function registerAction(Request $request)
+    {
+        return $this->render('default/register.html.twig', array(
+            
+        ));
+    }
+}
